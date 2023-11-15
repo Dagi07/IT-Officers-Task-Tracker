48531591ca0f0527651f1789fb7a4d6ecde0cfe1
Dagi07 <dagmawiamanuel@gmail.com>
2023-11-15 22:51:43 +0300

    Working on tasks to do later

--- frontend/src/components/Later.jsx
+++ frontend/src/components/Later.jsx
@@ -1,26 +1,50 @@
 import React from "react";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { MobileTimePicker } from "@mui/x-date-pickers/MobileTimePicker";
+import dayjs from "dayjs";
 
 const Later = () => {
   return (
-    <div>
+    <div className="later_container">
       <br />
       <br />
       <br />
       <h1>Tasks to be done later today</h1>
-      <form>
+      <form className="later_form">
         <textarea
+          className="form-control"
           name="task_detail"
           id="task_detail"
-          cols="30"
-          rows="10"
+          rows="2"
           placeholder="Task Detail"
         ></textarea>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DatePicker />
-        </LocalizationProvider>
+        <span>
+          <p>Estimated task completion time:</p>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <MobileTimePicker defaultValue={dayjs()} />
+          </LocalizationProvider>
+        </span>
+        <span>
+          <label htmlFor="done_by" className="form-label">
+            Task assignee:
+          </label>
+          <select
+            id="done_by"
+            // onChange={handleChange}
+            // value={taskadd.done_by}
+            name="done_by"
+            size="1"
+            className="form-select"
+          >
+            <option value="Sirak">Sirak</option>
+            <option value="Dagmawi">Dagmawi</option>
+            <option value="Tsegaye">Tsegaye</option>
+          </select>
+        </span>
+        <button className="btn btn-primary" type="submit">
+          Submit
+        </button>
       </form>
     </div>
   );
